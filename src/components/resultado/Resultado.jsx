a187a0dddb9f78782a5d27b37570313bca31006c
caio simplicio <caio1simplicio@gmail.com>
2025-03-23 21:34:57 -0300

    adjusting bmi decimal places

--- src/components/resultado/Resultado.jsx
+++ src/components/resultado/Resultado.jsx
@@ -1,7 +1,7 @@
 import './Resultado.modules.css'
 
 const Resultado = ({nome, altura, idade, peso, click}) => {
-    const alturaEmMetros = altura/100;
+    const alturaEmMetros = (altura/100);
     const imc = peso/(alturaEmMetros**2)
     let display;
     click ? display = 'flex' : display = 'none';
@@ -9,8 +9,8 @@ const Resultado = ({nome, altura, idade, peso, click}) => {
         <div className='popup' style={{display: display}}>
             <div className="texto">
                 <h1>Resultado: </h1>
-                <h4>{nome}, você tem {altura}M, {idade} anos, {peso}KG.</h4>
-                <h4>Seu IMC atualmente é de: {imc}</h4>
+                <h4>{nome}, você tem {alturaEmMetros}M, {idade} anos, {peso}KG.</h4>
+                <h4>Seu IMC atualmente é de: {imc.toFixed(2)}</h4>
                 <button type='reset'>Fechar</button>
             </div>
         </div>
